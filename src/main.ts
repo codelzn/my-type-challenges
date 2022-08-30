bd73fa818e26681d042d2d2fddae066dc24f2b0f
codelzn <lzn724958732@gmail.com>
2022-08-30 20:50:57 +0900

    challenge:3 and 4

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,40 @@ type HelloWorld = string;
 type test = Expect<Equal<HelloWorld, string>>;
 
 // Challenge 2
-interface Todo {
+interface Todo2 {
   title: string;
   description: string;
   completed: boolean;
 }
 
-type TodoPreview = MyPick<Todo, 'title' | 'completed'>;
+type TodoPreview = MyPick<Todo2, 'title' | 'completed'>;
 
-const todo: TodoPreview = {
+const todo2: TodoPreview = {
   title: 'Clean room',
   completed: false,
 };
 
-type MyPick<T, K extends keyof T> = { [P in K]: T[P] }
+type MyPick<T, K extends keyof T> = { [P in K]: T[P] };
+
+// challenge 3
+interface Todo3 {
+  title: string;
+  description: string;
+}
+type MyReadonly<T> = { readonly [P in keyof T]: T[P] };
+
+const todo3: MyReadonly<Todo3> = {
+  title: 'Hey',
+  description: 'foobar',
+};
+// todo3.title = "Hello" // Error: cannot reassign a readonly property
+// todo3.description = "barFoo" // Error: cannot reassign a readonly property
+
+// challenge 4
+const tuple4 = ['tesla', 'model 3', 'model X', 'model Y'] as const;
+
+type TupleToObject<T extends readonly (string | number | symbol)[]> = {
+  [K in T[number]]: K;
+};
+
+type result = TupleToObject<typeof tuple4>; // expected { tesla: 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
