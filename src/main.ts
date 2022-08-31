78b9280420a77a6de70c401d31e9d0cc8a4260ce
codelzn <lzn724958732@gmail.com>
2022-08-31 21:16:06 +0900

    challenge:5 and 6

--- src/main.ts
+++ src/main.ts
@@ -43,3 +43,24 @@ type TupleToObject<T extends readonly (string | number | symbol)[]> = {
 };
 
 type result = TupleToObject<typeof tuple4>; // expected { tesla: 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
+
+
+// challenge 5
+
+type arr1 = ['a', 'b', 'c']
+type arr2 = [3, 2, 1]
+
+type First<T extends any[]> = T extends [infer F, ...any] ? F : never
+
+type head1 = First<arr1> // expected to be 'a'
+type head2 = First<arr2> // expected to be 3
+
+// challenge 6
+
+type tesla = ['tesla', 'model 3', 'model X', 'model Y']
+type spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT']
+
+type Length<T extends readonly any[]> = T['length']
+
+type teslaLength = Length<tesla> // expected 4
+type spaceXLength = Length<spaceX> // expected 5
