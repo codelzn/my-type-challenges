269aa5bebbd45c87882c96760a7ed81942546c9e
codelzn <lzn724958732@gmail.com>
2022-09-01 20:10:19 +0900

    challenge:7 and 8

--- src/main.ts
+++ src/main.ts
@@ -44,23 +44,44 @@ type TupleToObject<T extends readonly (string | number | symbol)[]> = {
 
 type result = TupleToObject<typeof tuple4>; // expected { tesla: 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
 
-
 // challenge 5
 
-type arr1 = ['a', 'b', 'c']
-type arr2 = [3, 2, 1]
+type arr1 = ['a', 'b', 'c'];
+type arr2 = [3, 2, 1];
 
-type First<T extends any[]> = T extends [infer F, ...any] ? F : never
+type First<T extends any[]> = T extends [infer F, ...any] ? F : never;
 
-type head1 = First<arr1> // expected to be 'a'
-type head2 = First<arr2> // expected to be 3
+type head1 = First<arr1>; // expected to be 'a'
+type head2 = First<arr2>; // expected to be 3
 
 // challenge 6
 
-type tesla = ['tesla', 'model 3', 'model X', 'model Y']
-type spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT']
+type tesla = ['tesla', 'model 3', 'model X', 'model Y'];
+type spaceX = [
+  'FALCON 9',
+  'FALCON HEAVY',
+  'DRAGON',
+  'STARSHIP',
+  'HUMAN SPACEFLIGHT'
+];
 
-type Length<T extends readonly any[]> = T['length']
+type Length<T extends readonly any[]> = T['length'];
 
-type teslaLength = Length<tesla> // expected 4
-type spaceXLength = Length<spaceX> // expected 5
+type teslaLength = Length<tesla>; // expected 4
+type spaceXLength = Length<spaceX>; // expected 5
+
+// challenge 7
+// extends会自动结构然后一一比较
+type MyExclude<T, U extends T> = T extends U ? never : T;
+type Result7 = MyExclude<'a' | 'b' | 'c', 'a'>; // 'b' | 'c'
+
+// challenge 8
+type ExampleType = Promise<string>;
+
+type MyAwaited<T extends Promise<unknown>> = T extends Promise<infer X>
+  ? X extends Promise<unknown>
+    ? MyAwaited<X>
+    : X
+  : null;
+
+type Result8 = MyAwaited<ExampleType>; // string
