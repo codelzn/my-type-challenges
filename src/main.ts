a5644b50e1d468e9ba923dcebd22c812ddd4068c
codelzn <lzn724958732@gmail.com>
2022-09-07 18:16:03 +0900

    challenge

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,32 @@
 import './style.css';
 import { Expect, Equal } from '@type-challenges/utils';
 
+// challenges 1
+const fn = (v: boolean) => {
+  if (v) return 1;
+  else return 2;
+};
+
+type MyReturnType<T extends (...args: any) => any> = T extends (
+  ...args: any
+) => infer R
+  ? R
+  : never;
+
+type a = MyReturnType<typeof fn>; //  "1 | 2"
+
+// challenges 2
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+type MyOmit<T, K extends keyof T> = {
+  [P in keyof T as P extends K ? never : P]: T[P];
+};
+
+type TodoPreview = MyOmit<Todo, 'description' | 'title'>;
+
+const todo: TodoPreview = {
+  completed: false,
+};
